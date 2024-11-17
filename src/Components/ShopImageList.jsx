e7595ab9188d6d4e7586efd1188b6c231ac427ff
CMTrg <minhtruong2605@gmail.com>
2024-11-17 19:10:23 +0700

    changed again :(

--- src/Components/ShopImageList.jsx
+++ src/Components/ShopImageList.jsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-/* import Hat1 from "./assets/Shop/Hat1.svg";
-import Hat2 from "./assets/Shop/Hat2.svg";
-import Lanyard1 from "./src/assets/Shop/Lanyard1.svg";
-import Lightstick1 from "./src/assets/Shop/Lightstick1.svg";
-import Lightstick2 from "./src/assets/Shop/Lightstick2.svg";
-import Lightstick3 from "./src/assets/Shop/Lightstick3.svg";
-import Shirt1 from "./src/assets/Shop/Shirt1.svg";
-import Shirt2 from "./src/assets/Shop/Shirt2.svg"; */
+
 const ShopImageList = () => {
   // State to track the currently selected set of images
   const [selectedImages, setSelectedImages] = useState([]);
@@ -17,38 +10,38 @@ const ShopImageList = () => {
       id: 1,
       label: "ALL",
       images: [
-        "./Shop/Hat1.svg",
-        "./Shop/Hat2.svg",
-        "./Shop/Lanyard1.png",
-        "./Shop/Lightstick1.svg",
-        "./Shop/Lightstick2.svg",
-        "./Shop/Lightstick3.svg",
-        "./Shop/Shirt1.svg",
-        "./Shop/Shirt2.svg",
-      ], // Replace with your image paths
+        "/Shop/Hat1.svg",
+        "/Shop/Hat2.svg",
+        "/Shop/Lanyard1.png",
+        "/Shop/Lightstick1.svg",
+        "/Shop/Lightstick2.svg",
+        "/Shop/Lightstick3.svg",
+        "/Shop/Shirt1.svg",
+        "/Shop/Shirt2.svg",
+      ], // Image paths from public/Shop
     },
     {
       id: 2,
       label: "HAT",
-      images: ["./Shop/Hat1.svg", "./Shop/Hat2.svg"],
+      images: ["/Shop/Hat1.svg", "/Shop/Hat2.svg"],
     },
     {
       id: 3,
       label: "T-SHIRT",
-      images: ["./Shop/Shirt1.svg", "./Shop/Shirt2.svg"],
+      images: ["/Shop/Shirt1.svg", "/Shop/Shirt2.svg"],
     },
     {
       id: 4,
       label: "LANYARD",
-      images: ["./Shop/Lanyard1.png"],
+      images: ["/Shop/Lanyard1.png"],
     },
     {
       id: 5,
       label: "LIGHTSTICK",
       images: [
-        "./Shop/Lightstick1.svg",
-        "./Shop/Lightstick2.svg",
-        "./Shop/Lightstick3.svg",
+        "/Shop/Lightstick1.svg",
+        "/Shop/Lightstick2.svg",
+        "/Shop/Lightstick3.svg",
       ],
     },
   ];
